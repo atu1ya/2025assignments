ba0bd396bac92a84586eb3ae40a83bbac8652901
atu1ya <24225113@student.uwa.edu.au>
2025-05-19 20:11:16 +0800

    implementations and upload of math csv

--- javaproject/src/game/GameImpl.java
+++ javaproject/src/game/GameImpl.java
@@ -10,6 +10,9 @@ public class GameImpl implements Game {
     private PieceColour winner;
 
     public GameImpl(int size) {
+        if (size <= 0) {
+        throw new IllegalArgumentException("Size must be positive");
+        }
         this.size = size;
         this.grid = new GridImpl(size);
         this.currentPlayer = PieceColour.WHITE;
